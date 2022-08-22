cab32555bda3e73a19e4f69e1b738fae4653203a
AvelChoi <diavelcoop@gmail.com>
2022-08-22 23:15:56 +0900

    Lemmatization done

---
+++ 02. Text Processing/03) Stemming and Lemmatization.py
@@ -0,0 +1,13 @@
+from nltk import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+words = ['policy', 'doing', 'organization', 'have', 'going', 'love', 'lives', 'fly', 'dies', 'watched', 'has',
+         'starting']
+
+print("표제어 추출 전: ", words)
+print("표제어 추출 후:", [lemmatizer.lemmatize(word) for word in words])
+
+print(lemmatizer.lemmatize('dies', 'v'))
+print(lemmatizer.lemmatize('watched', 'v'))
+print(lemmatizer.lemmatize('has', 'v'))
