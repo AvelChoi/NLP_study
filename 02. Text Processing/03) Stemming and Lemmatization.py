7e97bf4f3f9f3329afeda0a212fc29b074c7ba4a
AvelChoi <diavelcoop@gmail.com>
2022-08-31 00:00:08 +0900

    Stemming and Lemmatization done
    어간 추출과 표제어 추출의 차이를 살펴보고, nltk 패키지를 이용해서 간단한 실습 수행
    
    todo: 04) 불용어

--- 02. Text Processing/03) Stemming and Lemmatization.py
+++ 02. Text Processing/03) Stemming and Lemmatization.py
@@ -1,4 +1,7 @@
 from nltk import WordNetLemmatizer
+from nltk import PorterStemmer
+from nltk import LancasterStemmer
+from nltk.tokenize import word_tokenize
 
 lemmatizer = WordNetLemmatizer()
 
@@ -11,3 +14,23 @@ print("표제어 추출 후:", [lemmatizer.lemmatize(word) for word in words])
 print(lemmatizer.lemmatize('dies', 'v'))
 print(lemmatizer.lemmatize('watched', 'v'))
 print(lemmatizer.lemmatize('has', 'v'))
+
+stemmer = PorterStemmer()
+
+sentence = "This was not the map we found in Billy Bones's chest, but an accurate copy, complete in all things--names " \
+           "and heights and soundings--with the single exception of the red crosses and the written notes. "
+
+tokenized_sentence = word_tokenize(sentence)
+
+print('어간 추출 전', tokenized_sentence)
+print('어간 추출 후', [stemmer.stem(word) for word in tokenized_sentence])
+
+# NLTK Lancaster Stemmer
+porter_stemmer = PorterStemmer()
+lancaster_stemmer = LancasterStemmer()
+
+words = ['policy', 'doing', 'organization', 'have', 'going', 'love', 'lives', 'fly', 'dies', 'watched', 'has',
+         'starting']
+print('어간추출')
+print('Porter stemmer:', [porter_stemmer.stem(w) for w in words])
+print('Lancaster stemmer:', [lancaster_stemmer.stem(w) for w in words])
